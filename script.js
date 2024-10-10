fa75236bc1c9ff67d5c5696bfb0bb80bd14137c4
Lhurdb19 <haryomidey15@gmail.com>
2024-10-10 23:48:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ window.addEventListener("load", ()=> {
 
 
 // Responsiveness (Hamburger)
-function hideSidebar() {
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "none" 
-}
-function showSidebar() {
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "flex";
-};
+
+const handburger = document.querySelector(".handburger")
+const navbar = document.querySelector(".navbar");
+
+
+handburger.addEventListener("click", (e) => {
+    e.preventDefault()
+    handburger.classList.toggle("active");
+    navbar.classList.toggle("active")
+});
+
+
+
+document.querySelectorAll(".navbar li").forEach(n => n.addEventListener("click", ()=>{
+    handburger.classList.remove("active");
+    navbar.classList.remove("active");
+}))
+
+
+//DROPDOWN FUNCTION()
 
 
 //Clock Display
